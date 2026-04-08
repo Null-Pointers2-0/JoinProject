048d9ca353d62b586cad2536cea1e3958357a975
gerard <gerymaps200515@gmail.com>
2026-04-08 20:30:16 +0200

    feat(url): add profile url

--- web_app/urls.py
+++ web_app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="identify/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path('profile/', views.profile, name='profile')
 
 ]
