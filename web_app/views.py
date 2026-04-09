553a433dc4d31c56a4379b4c8356508d0a4cdba4
Norbert <naguilerlavalira@gmail.com>
2026-04-09 10:29:23 +0200

    chore: medium part from user settings

--- web_app/views.py
+++ web_app/views.py
@@ -14,4 +14,11 @@ def register_view(request):
 def user_setting(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    return render(request, 'User/user_setting.html')
+    '''if request.user.is_client:
+        return render(request, 'User/user_types/user_client.html')
+    elif request.user.is_admin:
+        return render(request, 'User/user_types/user_admin.html')
+    elif request.user.is_tecnical:
+        return render(request, 'User/user_types/user_tecnical.html')'''
+    return render(request, 'User/user_types/user_client.html')
+    #return render(request, 'User/user_setting.html')
