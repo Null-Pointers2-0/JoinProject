55a6653558915f05bbba976a7ce30c8a5eadc871
gerard <gerymaps200515@gmail.com>
2026-04-13 15:19:04 +0200

    feat(user): update the url and view to new path

--- web_app/views.py
+++ web_app/views.py
@@ -20,5 +20,10 @@ def profile(request):
         return render(request, 'User/user_types/user_admin.html')
     elif request.user.is_tecnical:
         return render(request, 'User/user_types/user_tecnical.html')'''
-    return render(request, 'User/user_types/user_client.html')
+    return render(request, 'User/user_parts/profile.html')
     #return render(request, 'User/user_setting.html')
+
+def history(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'User/user_parts/history.html')
