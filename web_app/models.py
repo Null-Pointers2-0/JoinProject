bd8fa7dd1469ef99c2f42e625c85f7f116553064
Bogdan Nicolae Stefanescu <159245364+Bogdan2005-19@users.noreply.github.com>
2026-04-14 10:23:43 +0200

    Import settings from web module in models.py

--- web_app/models.py
+++ web_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from web import settings
 
 class API(models.Model):
     port = models.IntegerField(unique=True)
