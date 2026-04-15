3a37a6c91e8498f65d856a4be5fa4ed3a776aff1
Norbert <naguilerlavalira@gmail.com>
2026-04-15 10:50:52 +0200

    feat (catalog-homepage): aded the filtrering on catalog

--- web_app/urls.py
+++ web_app/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="identify/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-<<<<<<< HEAD
+
     #path('user/profile', views.profile, name='profile'),
     #path('user/history', views.history, name='history'),
-=======
-    path("catalog/", views.catalog_view, name="catalog"),
 
->>>>>>> 22-featsearch-implement-catalog-search-and-filtering-interface
+    path("catalog/", views.catalog_view, name="catalog"),
 ]
