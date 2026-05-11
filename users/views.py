24491fd3f0f4664a8a6a4dc0ddf929825c3210fc
gerard <gerymaps200515@gmail.com>
2026-05-11 11:03:06 +0200

    refactor(views): login requiret refactored and imports

--- users/views.py
+++ users/views.py
@@ -5,12 +5,11 @@ from .services import get_content_analytics, format_analytics_for_csv
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from web_app.forms import CustomUserChangeForm
-from web_app.models import Movie, Series, Contingut
+from web_app.models import Movie, Series, Contingut, API
 
 # Create your views here.
+@login_required(login_url='login')
 def profile(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
         
     if request.method == 'POST':
         form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
@@ -23,36 +22,28 @@ def profile(request):
 
     return render(request, 'users/profile/profile.html', {'form': form})
 
+@login_required(login_url='login')
 def history(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
     movies = Movie.objects.all()
     return render(request, 'users/parts/history.html', {'movies': movies})
 
+@login_required(login_url='login')
 def followed(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
     profile, _ = request.user.profile, True
     preferits_ids = profile.preferits.values_list('id', flat=True)
     movies = Movie.objects.filter(contingut_id__in=preferits_ids)
     series_list = Series.objects.filter(contingut_id__in=preferits_ids)
     return render(request, 'users/parts/followed.html', {'movies': movies, 'series_list': series_list})
 
-
+@login_required(login_url='login')
 def subscription(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
-    from web_app.models import API
-    from django.contrib import messages as django_messages
-
     all_apis = API.objects.all().order_by('port')
     user_subscription_ids = set(request.user.subscriptions.values_list('id', flat=True))
 
     if request.method == 'POST':
         selected_ids = request.POST.getlist('subscriptions')
         request.user.subscriptions.set(API.objects.filter(id__in=selected_ids))
-        django_messages.success(request, '¡Suscripciones actualizadas correctamente!')
+        messages.success(request, '¡Suscripciones actualizadas correctamente!')
         return redirect('suscription')
 
     return render(request, 'users/parts/subscription.html', {
@@ -63,7 +54,7 @@ def subscription(request):
 def is_admin_or_staff(user):
     return user.is_staff or user.type == 'Admin'
 
-@login_required
+@login_required(login_url='login')
 @user_passes_test(is_admin_or_staff)
 def export_analytics_csv(request):
     platform_id = request.GET.get('platform')
@@ -95,3 +86,9 @@ def export_analytics_csv(request):
         ])
     
     return response
+
+
+@login_required(login_url='login')
+@user_passes_test(is_admin_or_staff)
+def hola(request):
+    pass
