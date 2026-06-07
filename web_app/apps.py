325b170f74f02b99bb9bd73fa863476dbe8758f1
NorbertAguilera <naguileralavalira@gmail.com>
2026-06-07 19:35:37 +0200

    fix(ci): exclude tests/venv from static scan and fix hardcoded password

--- web_app/apps.py
+++ web_app/apps.py
@@ -41,7 +41,7 @@ def create_default_user(sender, **kwargs):
 
     username = 'admin'
     email = 'admin@admin.com'
-    password = 'adminpassword'
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(
